502bc4b02b0fb8ba9ee611fb323f305e2ee29659
ltsai1 <louie.tsai@intel.com>
2017-09-04 00:26:11 -0700

    [] add heat map

--- data_train.py
+++ data_train.py
@@ -12,6 +12,9 @@ from sklearn.preprocessing import StandardScaler
 import time
 from sklearn.svm import LinearSVC
 from sklearn.cross_validation import train_test_split
+
+#import pickle
+from scipy.ndimage.measurements import label
 #from skimage.feature import hog
 #from skimage import color, exposure
 # images are divided up into vehicles and non-vehicles
@@ -80,13 +83,6 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, featu
                        #visualise=False, feature_vector=feature_vec)
                        visualise=False)
         return features
-
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from sklearn.preprocessing import StandardScaler
-import glob
 
 # Define a function to compute binned color features  
 def bin_spatial(img, size=(32, 32)):
@@ -369,6 +365,36 @@ def search_windows(img, windows, clf, scaler, color_space='RGB',
     return on_windows
 
 
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap# Iterate through list of bboxes
+    
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
+
+def draw_labeled_bboxes(img, labels):
+    # Iterate through all detected cars
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
+    # Return the image
+    return img
 
     
 data_info = data_look(cars, notcars)
@@ -591,4 +617,35 @@ hot_windows = search_windows(image, windows, svc, X_scaler, color_space=color_sp
 window_img = draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)                    
 
 plt.imshow(window_img)
+plt.show()
+
+
+
+##############################
+# Read in image similar to one shown above 
+#image = mpimg.imread('test_image.jpg')
+box_list = hot_windows
+heat = np.zeros_like(image[:,:,0]).astype(np.float)
+
+# Add heat to each box in box list
+heat = add_heat(heat,box_list)
+    
+# Apply threshold to help remove false positives
+heat = apply_threshold(heat,1)
+
+# Visualize the heatmap when displaying    
+heatmap = np.clip(heat, 0, 255)
+
+# Find final boxes from heatmap using label function
+labels = label(heatmap)
+draw_img = draw_labeled_bboxes(np.copy(image), labels)
+
+fig = plt.figure()
+plt.subplot(121)
+plt.imshow(draw_img)
+plt.title('Car Positions')
+plt.subplot(122)
+plt.imshow(heatmap, cmap='hot')
+plt.title('Heat Map')
+fig.tight_layout()
 plt.show()
